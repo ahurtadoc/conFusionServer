a5f61e111c8da1152528993dbbb0def704486594
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-05-03 00:30:12 -0500

    Passport JWT

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authenticate = require("../authenticate");
 
 const  Leaders = require('../models/leaders');
 const leaderRouter = express.Router();
@@ -16,7 +17,7 @@ leaderRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post( (req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         Leaders.create(req.body)
             .then((leader) => {
                 console.log('Leader created', leader);
@@ -30,7 +31,7 @@ leaderRouter.route('/')
         res.statusCode = 403;
         res.end(`PUT operation not supported on /leaders`);
     })
-    .delete( (req, res, next) => {
+    .delete(authenticate.verifyUser, (req, res, next) => {
         Leaders.remove({})
             .then((resp) => {
                 res.statusCode = 200;
@@ -51,11 +52,11 @@ leaderRouter.route('/:leaderId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post( (req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         res.statusCode = 403;
         res.end(`POST operation not supported on /leaders`);
     })
-    .put( (req, res, next) => {
+    .put(authenticate.verifyUser, (req, res, next) => {
         Leaders.findByIdAndUpdate(req.params.leaderId,{
             $set: req.body
         },{new: true})
@@ -66,7 +67,7 @@ leaderRouter.route('/:leaderId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .delete( (req, res, next) => {
+    .delete(authenticate.verifyUser, (req, res, next) => {
         Leaders.findByIdAndRemove(req.params.leaderId)
             .then((resp) => {
                 res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const authenticate = require("../authenticate");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -27,9 +28,10 @@ router.post('/signup', function (req,res,next) {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+    let token = authenticate.getToken({_id: req.user._id})
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, status: 'You are successfully logged in!'});
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res,next) => {
