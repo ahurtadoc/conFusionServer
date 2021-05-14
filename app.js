0186d13bfd000d1356ccb438cf3caec0928ecb0d
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-05-13 21:02:12 -0500

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishesRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter')
 var leaderRouter = require('./routes/leaderRouter')
+var uploadRouter = require('./routes/uploadRouter')
 var app = express();
 
 const mongoose = require('mongoose');
@@ -52,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishesRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
