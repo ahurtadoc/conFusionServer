40f20ed34296b4889c1c2dd74dd5e22e0174f241
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-05-13 22:22:47 -0500

    Cors

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require("../authenticate");
+const cors = require('./cors')
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions,cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
     User.find({})
         .then((users) => {
             res.statusCode = 200;
@@ -15,7 +16,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req,
         .catch( (err) => {next(err)})
     });
 
-router.post('/signup', function (req,res,next) {
+router.post('/signup',cors.corsWithOptions, function (req,res,next) {
     User.register(new User({username: req.body.username}),
         req.body.password, (err, user) => {
             if(err) {
@@ -45,14 +46,14 @@ router.post('/signup', function (req,res,next) {
         });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
     let token = authenticate.getToken({_id: req.user._id})
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res,next) => {
+router.get('/logout',cors.cors, (req, res,next) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
