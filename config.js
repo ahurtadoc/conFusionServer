5aac5bbdf5e487d8e0a9d32be96ed8fb690bddab
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-05-14 23:24:58 -0500

    passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId : '3002068223413364',
+        clientSecret: '0e6d4c20259d1ba7860682d8bc7d3d7b'
+    }
 }
